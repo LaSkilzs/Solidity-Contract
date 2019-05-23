59871d05971fe2963af43af55f4b1e59dbec3ed3
La Fountain <github email address>
2019-05-22 20:56:02 -0400

    Update migrations folder

--- contract/migrations/1_initial_migration.js
+++ contract/migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
-const Migrations = artifacts.require("Migrations");
+const Migrations = artifacts.require("./Migrations.sol");
+const HelloEthSalon = artifacts.require("./HelloEthSalon.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(HelloEthSalon);
 };
